ba9a46641c48357a8a535c4e2154e59692476b92
Zilde Souto Maior Neto <zsmn@cin.ufpe.br>
2020-03-05 11:46:50 -0300

    Update ex1.hs

--- ex1.hs
+++ ex1.hs
@@ -1,7 +1,17 @@
 vendas n = n
 
+-- without list
+
 getVendas :: Int -> Int -> Int
 getVendas n s
-    | n == 0  = 0
+    | n < 0  = 0
     | (vendas n) == s = 1 + (getVendas (n-1) s)
     | otherwise = getVendas (n-1) s
+
+-- with list at the input
+
+getVendasList :: [Int] -> Int -> Int
+getVendasList n s
+    | n == []                =  0
+    | (head n) == s          =  1 + (getVendasList (tail n) s) 
+    | otherwise              =  getVendasList (tail n) s
